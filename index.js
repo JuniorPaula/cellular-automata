7136533b0a68aadb82c6bd71c32e253bbfca0497
Junior Paula <luke.junnior@icloud.com>
2023-05-03 20:41:32 -0300

    Create a board from an array cellular

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 "use strict";
+const BOARD_ROWS = 32;
+const BOARD_COLS = BOARD_ROWS;
+const board = [];
+for (let r = 0; r < BOARD_ROWS; ++r) {
+    board.push(new Array(BOARD_COLS).fill("dead"));
+}
 const canvasId = "app";
 const app = document.getElementById(canvasId);
 if (app === null) {
     throw new Error(`Could not found canvas ${canvasId}`);
 }
+app.width = 800;
+app.height = 800;
 const ctx = app.getContext("2d");
 if (ctx === null) {
     throw new Error(`Could not initialize 2d context`);
 }
-console.log(ctx);
+const CELL_WIDTH = app.width / BOARD_COLS;
+const CELL_HEIGHT = app.height / BOARD_ROWS;
+function render() {
+    if (!ctx)
+        return;
+    ctx.fillStyle = "#202020";
+    ctx.fillRect(0, 0, app.width, app.height);
+    ctx.fillStyle = "red";
+    for (let r = 0; r < BOARD_ROWS; ++r) {
+        for (let c = 0; c < BOARD_COLS; ++c) {
+            if (board[r][c] === "alive") {
+                const x = c * CELL_WIDTH;
+                const y = r * CELL_HEIGHT;
+                ctx.fillRect(x, y, CELL_WIDTH, CELL_HEIGHT);
+            }
+        }
+    }
+}
+app.addEventListener("click", (e) => {
+    const col = Math.floor(e.offsetX / CELL_WIDTH);
+    const row = Math.floor(e.offsetY / CELL_HEIGHT);
+    board[col][row] = "alive";
+    render();
+});
+render();
